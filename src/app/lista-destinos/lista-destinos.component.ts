d789b2a021f9a42c8d4693c0ccb74f594a9bec16
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-09-29 10:36:00 -0300

    Aplicación SPA Mejorada

--- src/app/lista-destinos/lista-destinos.component.ts
+++ src/app/lista-destinos/lista-destinos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DestinoViaje } from './../models/destino-viaje.model';
 
 @Component({
   selector: 'app-lista-destinos',
@@ -7,13 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaDestinosComponent implements OnInit {
 
-  destinos: string[];
+  destinos: DestinoViaje[];
 
   constructor() { 
-    this.destinos = ['Río de Janeiro', 'Lima', 'Buenos Aires', 'Barcelona'];
+    this.destinos = [];
    }
 
   ngOnInit(): void {
+  }
+
+  guardar(nombre:string, url:string):boolean {
+    this.destinos.push(new DestinoViaje(nombre, url));
+    
+    return false;
   }
 
 }
